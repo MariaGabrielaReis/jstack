0da72262ea0cba49de581dcdb5a47f6520acd8ff
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-02-18 20:41:57 -0300

    Chapter 3: modules

---
+++ chapter-3/modules/index.js
@@ -0,0 +1,9 @@
+/** IMPORTAÇÃO DE MÓDULOS
+ *
+ * const { printName, lastName } = require('./printName');
+ * printName(`Maby ${lastName}`);
+ */
+
+//IMPORTAÇÃO DE MÓDULOS NATIVOS
+const os = require('os');
+console.log(os.type());
