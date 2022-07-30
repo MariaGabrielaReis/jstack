a512e044729e1f593c653125d95b3c697afb8eef
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2022-07-30 18:06:37 -0300

    refactor: add method for response status and body

--- chapter-3/2-httpServer/src/controllers/UserController.js
+++ chapter-3/2-httpServer/src/controllers/UserController.js
@@ -9,20 +9,15 @@ module.exports = {
       return a.id > b.id ? 1 : -1;
     });
 
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(sortedUsers));
+    response.send(200, sortedUsers);
   },
   getUserById(request, response) {
     const { id } = request.params;
 
     const user = users.find(user => user.id === Number(id));
 
-    if (user) {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.end(JSON.stringify(user));
-    } else {
-      response.writeHead(400, { 'Content-Type': 'application/json' });
-      response.end(JSON.stringify({ error: 'User not found' }));
-    }
+    user
+      ? response.send(200, user)
+      : response.send(400, { error: 'User not found' });
   },
 };
