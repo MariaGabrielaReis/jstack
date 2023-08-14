5aae39147447e44630270f819a0fc555c30dc84c
Maria Gabriela Reis <mariagabrielagreis@gmail.com>
2023-08-14 12:17:36 -0300

    feat: configurate webpack mode and loaders

--- react-fundamentals/webpack.config.js
+++ react-fundamentals/webpack.config.js
@@ -2,21 +2,27 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = {
-  // path to root
-  entry: path.resolve(__dirname, "src", "index.js"),
-  output: {
-    // path to generate build
-    path: path.resolve(__dirname, "build"),
-    // random name because of browser cache
-    filename: "bundle[hash].js",
-  },
-  plugins: [
-    // lib to handle random build name
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
-    }),
-    // lib to clean older builds files
-    new CleanWebpackPlugin(),
-  ],
+module.exports = (env, argv) => {
+  return {
+    // path to root
+    entry: path.resolve(__dirname, "src", "index.js"),
+    output: {
+      // path to generate build
+      path: path.resolve(__dirname, "build"),
+      // random name because of browser cache
+      filename: "bundle[hash].js",
+    },
+    plugins: [
+      // lib to handle random build name
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "public", "index.html"),
+      }),
+      // lib to clean older builds files
+      new CleanWebpackPlugin(),
+    ],
+    module: {
+      rules: [{ test: /\.js$/, exclude: /node_modules/, use: "babel-loader" }],
+    },
+    mode: argv.mode,
+  };
 };
